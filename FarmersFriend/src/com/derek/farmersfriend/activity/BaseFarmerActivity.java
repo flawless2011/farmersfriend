3382756df40fa1cd52e5f63636ecce2767fbd22a
flawless2011 <dlawless@gmail.com>
2013-12-28 17:58:58 -0600

    Add first activity stub

---
+++ FarmersFriend/src/com/derek/farmersfriend/activity/BaseFarmerActivity.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright Derek Lawless 2013
+ */
+package com.derek.farmersfriend.activity;
+
+import android.app.Activity;
+
+/**
+ * Purpose: A base class to inject universal functionality
+ *
+ */
+public abstract class BaseFarmerActivity extends Activity {
+
+}
